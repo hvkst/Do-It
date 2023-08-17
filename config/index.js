97ae50fb696e05fba13728e1b530034be8cb9cf9
brnd hvkst <bernd@hvkst.com>
2023-08-17 21:18:40 +0200

    fixing public folder problems

--- config/index.js
+++ config/index.js
@@ -27,6 +27,14 @@ const path = require('path');
 
 // Middleware configuration
 module.exports = (app) => {
+  // Normalizes the path to the views folder
+  app.set('views', path.join(__dirname, '..', 'views'));
+  // Sets the view engine to handlebars
+  app.set('view engine', 'hbs');
+  // Handles access to the public folder
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+  // Handles access to the favicon
+  app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')));
   // In development environment the app logs
   app.use(logger('dev'));
 
@@ -38,17 +46,9 @@ module.exports = (app) => {
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(flash());
-  // Normalizes the path to the views folder
-  app.set('views', path.join(__dirname, '..', 'views'));
-  // Sets the view engine to handlebars
-  app.set('view engine', 'hbs');
-  // Handles access to the public folder
-  app.use(express.static(path.join(__dirname, '..', 'public')));
 
   app.use(passport.authenticate('session'));
 
-  // Handles access to the favicon
-  app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')));
   app.use(function (req, res, next) {
     res.locals.user = req.user;
     next();
